f6312040ec3ad57b88311db082e04f7103490079
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-06-16 13:56:24 -0400

    Added simple HUD

---
+++ BoatBoat/Assets/_Scripts/HUD.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+	public string personName;
+	public string levelTheme;
+	public GameObject hudGUI;
+
+	// Use this for initialization
+	void Start () {
+		hudGUI.guiText.text = personName + "\n" + levelTheme;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
