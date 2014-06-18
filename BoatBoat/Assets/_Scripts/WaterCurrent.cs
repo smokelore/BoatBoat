72b0147fa06a09201b5e782d12ba73e3a9c39482
Jory Folker <jfolker10@outlook.com>
2014-06-18 07:47:09 -0400

    A "Boat-load of changes"
    My end of the project is complete. Whenever you hit a wall, the
    "PINGAS!" sound effect plays.

---
+++ BoatBoat/Assets/_Scripts/WaterCurrent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterCurrent : MonoBehaviour {
+	Vector3 current;
+
+	// Use this for initialization
+	void Start () {
+		current = new Vector3 (-0.5f*Mathf.Sin(this.transform.rotation.y), 0.0f, 0.5f*Mathf.Cos(this.transform.rotation.y));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		audio.Play();
+	}
+	void OnTriggerStay(Collider other) {
+		//other.gameObject.rigidbody.AddForce(current);
+		if (other.attachedRigidbody) {
+			other.attachedRigidbody.AddForce(current);
+		}
+	}
+	void OnTriggerExit(Collider other) {
+		audio.Stop();
+	}
+}
