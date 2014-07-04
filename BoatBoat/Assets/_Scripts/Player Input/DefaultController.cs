6b7794ac4e72587f4a5975fe4716e0604a650ab9
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-07-03 22:50:31 -0400

    Controller Support prototype
    Can't get player to stay in WalkableArea collider...

---
+++ BoatBoat/Assets/_Scripts/Player Input/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using InControl;
+using System.Collections;
+
+public class DefaultController : InputController {
+	public GameObject BoatBoat;
+	public Collider WalkableArea;
+	public float speedFactor;
+
+	// Use this for initialization
+	void Start () {
+		InputManager.Setup();
+		SetPlayer(this.player);
+	}
+	
+	// Update is called once per frame
+	public override void Move () {
+		this.transform.rotation = WalkableArea.transform.rotation;
+		Vector3 movement = WalkableArea.transform.right * LeftStick.x * speedFactor + WalkableArea.transform.forward * LeftStick.y * speedFactor;
+		Vector3 newLocation = this.transform.position + movement*Time.deltaTime; 
+		if (WalkableArea.bounds.Contains(newLocation)) {
+			this.transform.position = newLocation;
+		}
+	}
+}
---
+++ BoatBoat/Assets/_Scripts/Player Input/InputController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using InControl;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+	public int player = -1;
+	private InputDevice device;
+	public string deviceName;
+
+	public Vector2 LeftStick;
+	public Vector2 RightStick;
+	public bool AButton;
+	public bool BButton;
+	public bool XButton;
+	public bool YButton;
+	public float LeftTrigger;
+	public float RightTrigger;
+
+	void Start() {
+		InputManager.Setup();
+
+		//SetPlayer(1);
+	}
+
+	void Update() {
+		if (HasPlayer()) {
+			InputManager.Update();
+			deviceName = device.Name;
+			LeftStick = new Vector2(device.LeftStickX, device.LeftStickY);
+			RightStick = new Vector2(device.RightStickX, device.RightStickY);
+			AButton = device.GetControl(InputControlType.Action1).IsPressed;
+			BButton = device.GetControl(InputControlType.Action2).IsPressed;
+			XButton = device.GetControl(InputControlType.Action3).IsPressed;
+			YButton = device.GetControl(InputControlType.Action4).IsPressed;
+			LeftTrigger = device.GetControl(InputControlType.LeftTrigger).Value;
+			RightTrigger = device.GetControl(InputControlType.RightTrigger).Value;
+			Move();
+		} else {
+			LeftStick = Vector2.zero;
+			RightStick = Vector2.zero;
+			AButton = false;
+			BButton = false;
+			XButton = false;
+			YButton = false;
+			LeftTrigger = 0f;
+			RightTrigger = 0f;
+		}
+	}
+
+	public virtual void Move() {
+
+	}
+
+	public void SetPlayer(int player) {
+		Debug.Log(InputManager.Devices.Count);
+		if (player < 4 && player >= 0) {
+			this.player = player;
+			this.device = InputManager.Devices[player];
+		} else {
+			this.player = -1;
+			this.device = null;
+		}
+	}
+
+	public void ResetPlayer() {
+		SetPlayer(-1);
+	}
+
+	public bool HasPlayer() {
+		return ((this.player < 4 && this.player >= 0) && this.device != null);
+	}
+}
