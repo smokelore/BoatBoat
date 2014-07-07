e55e2e12cfd623197ef0e3e9bd9dd1ec61a64440
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-07-07 18:54:08 -0400

    Stick detection for both oars

--- BoatBoat/Assets/_Scripts/Player Input/OarController.cs
+++ BoatBoat/Assets/_Scripts/Player Input/OarController.cs
@@ -13,6 +13,15 @@ public class OarController : InputController {
 	public bool leftOarEnded;
 	public float leftOarInitial;
 	public float leftOarFinal;
+	public float leftOarPrevious;
+	public float leftOarCurrent;
+
+	public bool rightOarStarted;
+	public bool rightOarEnded;
+	public float rightOarInitial;
+	public float rightOarFinal;
+	public float rightOarPrevious;
+	public float rightOarCurrent;
 
 	// Use this for initialization
 	void Start () {
@@ -21,34 +30,92 @@ public class OarController : InputController {
 	
 	// Move is called once per frame
 	public override void Move () {
-		if (leftOarEnded) {
-			if (leftOarStarted) {
-				rowFactor = leftOarFinal - leftOarInitial;
-				Debug.Log(rowFactor);
+		leftOarPrevious = leftOarCurrent;
+		leftOarCurrent = LeftStick.y;
+
+		if (leftOarStarted) {
+			if (leftOarEnded) {
+				// row finished
+				if (leftOarFinal > leftOarInitial) {
+					Row(false, Mathf.Pow((leftOarFinal - leftOarInitial)/2, 2));
+				}
 				leftOarStarted = false;
-			} else if (LeftTrigger == 0f) {
-				leftOarEnded = false;
-			}
-		} else if (leftOarStarted) {
-			if (LeftTrigger > leftOarFinal) {
-				leftOarFinal = LeftTrigger;
 			} else {
-				leftOarEnded = true;
+				// row in progress
+				if (leftOarCurrent >= leftOarPrevious) {
+					// still rowing
+					leftOarFinal = leftOarCurrent;
+				} else {
+					// stopped rowing
+					leftOarEnded = true;
+				}
 			}
 		} else {
-			if (LeftTrigger > 0) {
-				leftOarStarted = true;
-				leftOarEnded = false;
-				leftOarInitial = LeftTrigger;
-				leftOarFinal = -1f;
+			if (leftOarEnded) {
+				// row needs resetting
+				if (leftOarCurrent > leftOarPrevious) {
+					// new row started before last row reset
+					leftOarStarted = true;
+					leftOarEnded = false;
+					leftOarInitial = leftOarCurrent;
+				}
 			} else {
-				leftOarStarted = false;
+				// row already reset
+				if (leftOarCurrent > 0) {
+					// new row started
+					leftOarStarted = true;
+					leftOarInitial = leftOarCurrent;
+				}
+			}
+		}
+
+		rightOarPrevious = rightOarCurrent;
+		rightOarCurrent = RightStick.y;
+
+		if (rightOarStarted) {
+			if (rightOarEnded) {
+				// row finished
+				if (rightOarFinal > rightOarInitial) {
+					Row(true, Mathf.Pow((rightOarFinal - rightOarInitial)/2, 2));
+				}
+				rightOarStarted = false;
+			} else {
+				// row in progress
+				if (rightOarCurrent >= rightOarPrevious) {
+					// still rowing
+					rightOarFinal = rightOarCurrent;
+				} else {
+					// stopped rowing
+					rightOarEnded = true;
+				}
+			}
+		} else {
+			if (rightOarEnded) {
+				// row needs resetting
+				if (rightOarCurrent > rightOarPrevious) {
+					// new row started before last row reset
+					rightOarStarted = true;
+					rightOarEnded = false;
+					rightOarInitial = rightOarCurrent;
+				}
+			} else {
+				// row already reset
+				if (rightOarCurrent > 0) {
+					// new row started
+					rightOarStarted = true;
+					rightOarInitial = rightOarCurrent;
+				}
 			}
 		}
 		
 
-		if (RightTrigger > 0) {
+	}
 
+	public void Row(bool right, float amount) {
+		if (right) {
+			Debug.Log("Player " + player.playerNum + " rowed right oar: " + amount);
+		} else {
+			Debug.Log("Player " + player.playerNum + " rowed left oar: " + amount);
 		}
 	}
 
