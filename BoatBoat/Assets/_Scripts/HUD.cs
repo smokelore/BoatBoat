f401efaac03f6860290db75d25486c9a82c8ee83
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-07-22 01:06:29 -0400

    health bar unfinished

--- BoatBoat/Assets/_Scripts/HUD.cs
+++ BoatBoat/Assets/_Scripts/HUD.cs
@@ -6,6 +6,7 @@ public class HUD : MonoBehaviour {
 	public string levelTheme;
 	public GameObject hudGUI;
 	public bool winState;
+	public Texture healthBarTexture;
 
 	// Use this for initialization
 	void Start () {
@@ -26,5 +27,25 @@ public class HUD : MonoBehaviour {
 		}
 
 		hudGUI.guiText.text = personName + "\n" + levelTheme;
+	}
+
+	void OnGUI() {
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		GameObject player = GameObject.FindWithTag("Player");
+
+		foreach (GameObject enemy in enemies) {
+			Vector3 healthBarWorldPosition = enemy.transform.position + Vector3.up * 0f;
+			float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
+			if (distance < 50f) {
+				if (distance > 40f) {
+					distance = 40f;
+				}
+				int length = (int)(distance/40) * 100;
+				int width = (int)(distance/40) * 20;
+
+				Vector2 healthBarScreenPosition = Camera.main.WorldToScreenPoint(healthBarWorldPosition);
+				GUI.DrawTexture(new Rect(healthBarWorldPosition.x - (int)(length/2), healthBarWorldPosition.y + (int)(width/2), length, width), healthBarTexture, ScaleMode.StretchToFill, true, 0);
+			}
+		}
 	}
 }
