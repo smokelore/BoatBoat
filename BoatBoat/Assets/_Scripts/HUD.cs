a59fb4e6c68431e4af0213900f075ba1ced8da67
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-07-28 15:05:38 -0400

    HUD health works

--- BoatBoat/Assets/_Scripts/HUD.cs
+++ BoatBoat/Assets/_Scripts/HUD.cs
@@ -32,17 +32,16 @@ public class HUD : MonoBehaviour {
 		GameObject player = GameObject.FindWithTag("Player");
 
 		foreach (GameObject enemy in enemies) {
-			Vector3 healthBarWorldPosition = enemy.transform.position + Vector3.up * 0f;
-			float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
+			float distance = Vector3.Distance(Camera.main.transform.position, enemy.transform.position);
 			if (distance < 50f) {
-				if (distance > 40f) {
-					distance = 40f;
-				}
-				int length = (int)(distance/40) * 100;
-				int width = (int)(distance/40) * 20;
+				distance = Mathf.Clamp(distance, 10, 50);
+				Vector3 healthBarWorldPosition = enemy.transform.position + Vector3.up * (1.5f + (distance-10)/20);
+				int length = (int)(Mathf.Pow(2-distance/40,2) * enemy.GetComponent<Ship>().health) ;
+				int width = (int)(Mathf.Pow(2-distance/40,2) * 10);
 
 				Vector2 healthBarScreenPosition = Camera.main.WorldToScreenPoint(healthBarWorldPosition);
-				//GUI.DrawTexture(new Rect(healthBarWorldPosition.x - (int)(length/2), healthBarWorldPosition.y + (int)(width/2), length, width), healthBarTexture, ScaleMode.StretchToFill, true, 0);
+				Debug.Log((healthBarScreenPosition.x - (int)(length/2)) + " " + (healthBarScreenPosition.y - (int)(width/2)));
+				GUI.DrawTexture(new Rect(healthBarScreenPosition.x - (int)(length/2), Screen.height - healthBarScreenPosition.y + (int)(width/2), length, width), healthBarTexture, ScaleMode.StretchToFill, true, 0);
 			}
 		}
 	}
