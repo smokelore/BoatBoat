bb8dc41e66c9cc7d0591ce8eb2b77f765b9b96a2
Jory Folker <jfolker10@outlook.com>
2014-07-30 05:22:29 -0400

    Start Screen
    An intro screen with the name of the game and "PRESS START" is now in
    the game. It uses ocean water with the wave effect we have.

---
+++ BoatBoat/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture logo;
+	public Texture pressStart;
+	// Use this for initialization
+	
+	public int textHeight;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Application.LoadLevel("playtestScene");
+		}
+	}
+
+	void OnGUI(){
+		GUI.DrawTexture(new Rect(Screen.width/3, 16, Screen.width/3, textHeight), logo, ScaleMode.StretchToFill, true, 0);
+		GUI.DrawTexture(new Rect(Screen.width/3, Screen.height-(textHeight+16), Screen.width/3, textHeight), pressStart, ScaleMode.StretchToFill, true, 0);
+	}
+}
