a1089f4bcec1a5d6b3e86974d848d7764ebb329a
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-07-30 18:55:47 -0400

    started Merging Cole's treasure

---
+++ BoatBoat/Assets/_Scripts/enemyEnabler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyEnabler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(Transform child in this.transform){
+			if(Vector3.Distance (child.transform.position, Camera.main.transform.position) > 125){
+				child.gameObject.SetActive (false);
+			}else{
+				child.gameObject.SetActive (true);
+			}
+		}
+	}
+}
