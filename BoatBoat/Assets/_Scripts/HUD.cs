72f66023f79d3eb0975348edb65cf153c29b9f0d
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-07-31 18:55:50 -0400

    comment headers

--- BoatBoat/Assets/_Scripts/HUD.cs
+++ BoatBoat/Assets/_Scripts/HUD.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿// TEAM:
+// Mighty Morphin Pingas Rangers
+// Sebastian Monroy - sebash@gatech.edu - smonroy3
+// Thomas Cole Carver - tcarver3@gatech.edu - tcarver3
+// Chase Johnston - cjohnston8@gatech.edu - cjohnston8
+// Jory Folker - jfolker10@outlook.com - jfolker3
+using UnityEngine;
 using System.Collections;
 
 public class HUD : MonoBehaviour {
--- BoatBoat/Assets/_Scripts/enemyController.cs
+++ BoatBoat/Assets/_Scripts/enemyController.cs
@@ -1,8 +1,9 @@
-﻿// Sebastian Monroy - sebash@gatech.edu - smonroy3
+﻿// TEAM:
+// Mighty Morphin Pingas Rangers
+// Sebastian Monroy - sebash@gatech.edu - smonroy3
 // Thomas Cole Carver - tcarver3@gatech.edu - tcarver3
 // Chase Johnston - cjohnston8@gatech.edu - cjohnston8
 // Jory Folker - jfolker10@outlook.com - jfolker3
-
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
